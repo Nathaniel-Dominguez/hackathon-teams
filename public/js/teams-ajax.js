6809d95417f6f9b30031125775ba310276de8b66
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2018-08-16 15:58:17 -0700

    Created PUT and DELETE methods like we learned in class the last two days. Finally getting the hang of it a little bit even tho I'm copying our code alongs

--- public/js/teams-ajax.js
+++ public/js/teams-ajax.js
@@ -1,3 +1,36 @@
 $(document).ready(function() {
+	$('#delete-btn').on('click', function(e) {
+		e.preventDefault();
 
+		var teamElement = $(this);
+		var teamUrl = teamElement.attr('href');
+
+		$.ajax({
+			method: 'DELETE',
+			url: teamUrl
+		}).done(function(data) {
+			console.log('success', data);
+			window.location = '/teams'
+		}).fail(function(err) {
+			console.log('Error:', err);
+		});
+	});
+
+	$('#edit-form').on('submit', function(e) {
+		e.preventDefault();
+
+		var teamElement = $(this);
+		var teamUrl = $(this).attr('action');
+		var teamData = $(this).serialize();
+
+		$.ajax({
+			method: 'PUT',
+			url : teamUrl,
+			data: teamData
+		}).done(function(data) {
+			window.location = '/teams' 
+		}).fail(function(err) {
+			console.log('Error:', err)
+		});
+	});
 });
